54b6086919759e2ec56970217d7afea6f543f9c3
lnkrr <lnkrr@proton.me>
2026-05-21 12:14:09 +0300

    feat: add config options

--- gibberish.plugin.zsh
+++ gibberish.plugin.zsh
@@ -1,11 +1,49 @@
-local from="йцукенгшщзхъфывапролджэячсмитьбюёЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ\"№;:?.,/"
-local to="qwertyuiop[]asdfghjkl;'zxcvbnm,.\`QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>~@#\$^&/?|"
+local ycuken="йцукенгшщзхъфывапролджэячсмитьбюё\
+ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ\
+\"№;:?.,/"
+
+local qwerty="qwertyuiop[]asdfghjkl;'zxcvbnm,.\`\
+QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>~@#\$^&/?|"
+
+_gibberish_error() {
+    echo "gibberish: error: $1" >&2
+}
+
+_gibberish_get_layout() {
+    case "$1" in
+        "qwerty")
+            echo "$qwerty"
+            ;;
+        "ycuken" | "йцукен")
+            echo "$ycuken"
+            ;;
+        *)
+            _gibberish_error "invalid layout: '$1'"
+            ;;
+    esac
+}
+
+if [[ ! -v GIBBERISH_MAP_FROM ]]; then
+    export GIBBERISH_MAP_FROM="$ycuken"
+fi
+
+if [[ ! -v GIBBERISH_MAP_TO ]]; then
+    export GIBBERISH_MAP_TO="$qwerty"
+fi
+
+if [[ -v GIBBERISH_FROM ]]; then
+    export GIBBERISH_MAP_FROM="$(_gibberish_get_layout "$GIBBERISH_FROM")"
+fi
+
+if [[ -v GIBBERISH_TO ]]; then
+    export GIBBERISH_MAP_TO="$(_gibberish_get_layout "$GIBBERISH_TO")"
+fi
 
 typeset -gA _gibberish_map
 local i
 
-for (( i=1; i <= $#from; ++i )); do
-    _gibberish_map[${from[i]}]="${to[i]}"
+for (( i=1; i <= $#GIBBERISH_MAP_FROM; ++i )); do
+    _gibberish_map[${GIBBERISH_MAP_FROM[i]}]="${GIBBERISH_MAP_TO[i]}"
 done
 
 _gibberish_fix() {
@@ -52,7 +90,7 @@ command_not_found_handler() {
     echo
 
     case "$reply" in
-        [YyНн])
+        [Yy])
             ;;
         *)
             return
